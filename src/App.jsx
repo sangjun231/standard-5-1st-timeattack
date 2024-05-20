974d59feae6e163eccbd19913d12eb4b0bcae423
sangjun <987654321123sin@gmail.com>
2024-05-20 15:52:36 +0900

    계산기 덧셀 뺄셈 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,72 @@
+import { useState } from "react";
+
 export default function App() {
+  const [cr, setCr] = useState(0);
+  const [newNumber, setNewNumber] = useState("");
+
+  const addNumber = () => {
+    if (newNumber === "") {
+      setNewNumber("");
+      alert("값을 입력하세요.");
+      return;
+    }
+    setCr(cr + parseInt(newNumber));
+    setNewNumber("");
+  };
+
+  const subNumber = () => {
+    if (newNumber === "") {
+      setNewNumber("");
+      alert("값을 입력하세요.");
+      return;
+    }
+    setCr(cr - parseInt(newNumber));
+    setNewNumber("");
+  };
+
+  const initNumber = () => {
+    setCr(0);
+    setNewNumber("");
+  };
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input
+          type="text"
+          value={newNumber}
+          onChange={(event) => {
+            setNewNumber(event.target.value);
+          }}
+        />{" "}
+        만큼을{" "}
+        <button
+          onClick={() => {
+            addNumber();
+          }}
+        >
+          더할게요
+        </button>{" "}
+        <button
+          onClick={() => {
+            subNumber();
+          }}
+        >
+          뺄게요
+        </button>
+        <button
+          onClick={() => {
+            initNumber();
+          }}
+        >
+          초기화
+        </button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{cr}</p>
       </div>
     </div>
   );
